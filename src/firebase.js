b0c82dc33a1e1e6e4c10e17637a07e031b46d04e
Ayushi Arya <ayushi010btece22@igdtuw.ac.in>
2025-07-23 19:11:32 +0530

    Removed hardcoded Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAIMWEhJCdmBF9w2ppCQz4xP6MfFotPd2M",
-  authDomain: "placement-portal-ee2da.firebaseapp.com",
-  projectId: "placement-portal-ee2da",
-  storageBucket: "placement-portal-ee2da.firebasestorage.app",
-  messagingSenderId: "685446955990",
-  appId: "1:685446955990:web:3afebfb0fb85aeef3fda77",
-  measurementId: "G-H4G9PEQ4F8"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
